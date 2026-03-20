6d8af66c1e3d0336564a4c2488f0208f237c13f9
wygong-0525 <wanyinggong0525@gmail.com>
2026-03-20 22:03:13 +0000

    Create insert_sample_data.sql

---
+++ schema/insert_sample_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO GL_LEDGERS (
+  LEDGER_ID,
+  LEDGER_NAME,
+  DESCRIPTION,
+  LEDGER_CATEGORY_CODE
+)
+VALUES
+(1001, 'UK_PRIMARY', 'UK_PRIMARY_LEDGER', 'PRIMARY'),
+(1002, 'EU_PRIMARY', 'EU_PRIMARY_LEDGER', 'PRIMARY');
+
+INSERT INTO FND_USER (
+    USER_ID,
+    USER_NAME,
+    USER_DESCRIPTION
+)
+VALUES
+(101, 'FIN_USER_1', 'Finance user manual postings'),
+(102, 'FIN_USER_2', 'Finance user recurring journals'),
+(201, 'SYSTEM_USER', 'System generated journals'),
+(301, 'UPLOAD_USER', 'Spreadsheet upload user');
+
