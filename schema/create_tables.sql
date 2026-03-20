f55f991812cfe8529c3ee08268b108e09959507a
wygong-0525 <wanyinggong0525@gmail.com>
2026-03-20 20:16:32 +0000

    Create create_tables.sql

---
+++ schema/create_tables.sql
@@ -0,0 +1,14 @@
+create table GL_JE_Lines (
+  JE_HEADER_ID            INTEGER,
+  JE_LINE_NUM             INTEGER,
+  CODE_COMBINATION_ID     INTEGER,
+  ENTERED_DR              DECIMAL(18,2)
+  ENTERED_CR              DECIMAL(18,2)
+  ACCOUNTED_DR            DECIMAL(18,2)
+  ACCOUNTED_CR            DECIMAL(18,2)
+  CURRENCY_CODE           VARCHAR(10)
+  DESCRIPTION             VARCHAR(255)
+  );
+
+
+  
