f13dfe16fcb09a5d3a1664b04dce91f6ef28b533
wygong-0525 <wanyinggong0525@gmail.com>
2026-03-25 22:47:03 +0000

    Update V2 - Journal_Translation.sql

--- SQL/V2 - Journal_Translation.sql
+++ SQL/V2 - Journal_Translation.sql
@@ -1,1 +1,3 @@
-/* =====================================================================================================
+/* =========================================================================================
+Script Name: Journal_Transformation.SQL
+Purpose   : Build an analysis-ready journal population from Oracle-Style relational tables
