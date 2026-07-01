e6ecb47d082ffe0475272324184151661e621135
wygong-0525 <wanyinggong0525@gmail.com>
2026-07-01 22:14:50 +0100

    Update Financial_Reconciliation_Details.sql

--- SQL/Financial_Reconciliation_Details.sql
+++ SQL/Financial_Reconciliation_Details.sql
@@ -1,1 +1,13 @@
+/* ================================================================================================================
+Script Name: financial_reconciliation_detail.sql
 
+Purpose:
+Produce account-level reconciliation detail between journal movement and trial balance position in both local currency and
+USD equivalent.
+
+Inputs:
+1. USD_TRANSLATED_JOURNAL_POPULATION
+2. GL_BALANCES
+3. GL_JE_CODE_COMBINATIONS
+4. ENTITY_CURRENCY_MAPPING
+5. FX_RATES_STANDARDIZED
